2ec43e3db4891efd1e05a11eb7f3c77ec4fed90c
arife-senol <avcisenolarife@gmail.com>
2024-06-23 03:28:29 +0300

    Order page created

---
+++ src/pages/OrderFormPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './OrderFormPage.css'
+import axios from 'axios'
+import {Form, FormGroup, Label, Input, Button} from 'reactstrap'
+import { useHistory } from 'react-router-dom'
+export default function OrderFormPage() {
+  return (
+    <div>OrderPage</div>
+  )
+}

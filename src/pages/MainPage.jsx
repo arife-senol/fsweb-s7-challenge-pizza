9416f836e929939b2582a75ca24d8316279ecbb8
arife-senol <avcisenolarife@gmail.com>
2024-06-24 00:10:13 +0300

    project configuration done

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import { useHistory } from 'react-router-dom'
 import "./MainPage.css"
 export default function MainPage() {
-
+    let history = useHistory()
+function handleClick() {
+    history.push("./orderForm")
+}
   return (
     <div className='main-page'>
         <main className='main-page-section'>
@@ -11,7 +14,7 @@ export default function MainPage() {
               PİZZA, DOYURUR  
             </h1>
             <div className='ordered-btn-div'>
-                <button className='ordered-btn'> 
+                <button className='ordered-btn' onClick={handleClick}> 
                     ACIKTIM</button>
             </div>
         </main>

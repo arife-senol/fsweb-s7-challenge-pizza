4eef323cac9bd53ad3004460bb564f145d5976b0
arife-senol <avcisenolarife@gmail.com>
2024-06-23 03:29:32 +0300

    end page created

---
+++ src/pages/EndPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './EndPage.css'
+export default function EndPage() {
+  return (
+    <div className='endPageDiv'>
+        <p>TEBRİKLER!</p>
+        <p>SİPARİŞİNİZ ALINDI!</p>
+    </div>
+  )
+}
